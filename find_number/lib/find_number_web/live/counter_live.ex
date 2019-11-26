3656972d6b73275c7b2fb3adc85b0585ae109bcf
Yannis Weishaupt <yannis.weishaupt@gmail.com>
2019-11-26 19:55:03 +0100

    Step 0

--- find_number/lib/find_number_web/live/counter_live.ex
+++ find_number/lib/find_number_web/live/counter_live.ex
@@ -3,7 +3,31 @@ defmodule FindNumberWeb.CounterLive do
 
   def render(assigns) do
     ~L"""
-    <h1>Counter</h1>
+    <h1>Countasdasder</h1>
+
+    <%= @value %>
+
+    <button phx-click="increment">increment</button>
+    <button phx-click="decrement">decrement</button>
     """
   end
+
+  def mount(_session, socket) do
+    {:ok, assign(socket, value: 0)}
+  end
+
+  def handle_event("increment", _, %{assigns: %{value: value}} = socket) do
+    socket = assign(socket, :value, value + 1)
+    {:noreply, socket}
+  end
+
+  def handle_event("decrement", _, %{assigns: %{value: value}} = socket) do
+    socket = assign(socket, :value, value - 1)
+    {:noreply, socket}
+  end
+
+  # def handle_info(:update, socket) do
+  #   {:ok, temperature} = Thermostat.get_reading(socket.assigns.id)
+  #   {:noreply, assign(socket, :temperature, temperature)}
+  # end
 end
